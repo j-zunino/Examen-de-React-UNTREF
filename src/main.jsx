703e3ea22652ca1446cf1ad2c35f1fc82d01470e
j-zunino <zuninojuanbautista@gmail.com>
2024-12-05 15:50:26 -0300

    feat: add routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/movie/:id" element={<h1>place holder</h1>} />
+                <Route path="*" element={<h1>404 - Page not found</h1>} />
+            </Routes>
+        </BrowserRouter>
     </StrictMode>
 );
